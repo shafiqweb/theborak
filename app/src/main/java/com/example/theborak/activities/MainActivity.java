e541352514b3dcfdd4bb342524ca4af46d7b0f61
shafiqweb <45932468+shafiqweb@users.noreply.github.com>
2020-01-11 00:38:41 +0600

    worked on API in main activity

--- app/src/main/java/com/example/theborak/activities/MainActivity.java
+++ app/src/main/java/com/example/theborak/activities/MainActivity.java
@@ -14,10 +14,15 @@ import android.widget.Toast;
 import com.example.theborak.R;
 import com.example.theborak.connections.RetrofitConnectivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +43,29 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
                     String responseMessage = response.body().string();
-                    Toast.makeText(context,"response = "+responseMessage,Toast.LENGTH_SHORT).show();
+                    JSONObject jsonObject = new JSONObject(responseMessage);
+                    JSONArray jsonArray = jsonObject.getJSONArray("requests");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        String name = obj.getString("name");
+                        if (name.equals("Get All Movies")){
+                            String url = obj.getString("url");
+                            String auth = obj.getString("auth");
+                            JSONObject object = new JSONObject(auth);
+                            String type = object.getString("type");
+                            JSONArray bearerJsonArray = object.getJSONArray("bearer");
+                            for (int j = 0; j < bearerJsonArray.length(); j++){
+                                JSONObject bearerJSONObject = bearerJsonArray.getJSONObject(j);
+                                String key = bearerJSONObject.getString("key");
+                                String value = bearerJSONObject.getString("value");
+                            }
+                        }
+
+                    }
+
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
